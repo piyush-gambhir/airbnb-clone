176b4f3c7e59f7bab454a160097eca9bb8ff07e1
Piyush Gambhir <90608533+Piyush-Gambhir@users.noreply.github.com>
2023-08-08 02:09:04 +0530

    Small Updates

---
+++ app/components/Modals/LoginModal/config.tsx
@@ -0,0 +1,31 @@
+export const DATA = {
+  closeIcon: {
+    source: "/icons/common/close_icon_grey.svg",
+    altText: "close icon",
+  },
+};
+
+export const LOGIN_MODAL_DATA = {
+  title: "Log in or sign up",
+  description:
+    "We’ll call or text you to confirm your number. Standard message and data rates apply.",
+  button: "Continue",
+
+  socialLogin: [
+    {
+      title: "Continue with Google",
+      source: "/icons/common/google_icon.svg",
+      altText: "google icon",
+    },
+    {
+      title: "Continue with Facebook",
+      source: "/icons/common/facebook_icon.svg",
+      altText: "facebook icon",
+    },
+    {
+      title: "Continue with Apple",
+      source: "/icons/common/apple_icon.svg",
+      altText: "apple icon",
+    },
+  ],
+};
